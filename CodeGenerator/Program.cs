a64bbee97d180e57e627a29cdbff8ae7d1d26387
sinanfen <sinanfen@hotmail.com>
2024-08-01 15:59:42 +0300

    ToCamelCase() method created for the provided EntityName to camelCase

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -35,16 +35,17 @@
 
     static string GenerateInterface(string entityName, string moduleName)
     {
+        var camelCaseEntityName = ToCamelCase(entityName);
         return $@"
-        using Microsoft.EntityFrameworkCore.Query;     
-        using MODISO.DOMAIN.DTOs.{moduleName}.{entityName}Dtos;
-        using MODISO.DOMAIN.Entities.{moduleName};
-        using NArchitecture.Core.Persistence.Paging;
-        using MODISO.CORE.Utilities.Results.Abstract;
-        using System.Linq.Expressions;
-        
-        namespace MODISO.BLL.Abstract.{moduleName};
-
+    using Microsoft.EntityFrameworkCore.Query;     
+    using MODISO.DOMAIN.DTOs.{moduleName}.{entityName}Dtos;
+    using MODISO.DOMAIN.Entities.{moduleName};
+    using NArchitecture.Core.Persistence.Paging;
+    using MODISO.CORE.Utilities.Results.Abstract;
+    using System.Linq.Expressions;
+    
+    namespace MODISO.BLL.Abstract.{moduleName}
+    {{
         public interface I{entityName}Service
         {{
             Task<{entityName}Dto?> GetAsync(
@@ -66,44 +67,47 @@
             );
             Task<{entityName}Dto> GetByIdAsync(Guid id, CancellationToken cancellationToken);
             Task<IList<{entityName}Dto>> GetAllAsync(CancellationToken cancellationToken, int index = 0, int size = int.MaxValue);
-            Task<IDataResult<{entityName}Dto>> AddAsync({entityName}AddDto {entityName.ToLower()}AddDto, CancellationToken cancellationToken);
-            Task<IDataResult<{entityName}Dto>> UpdateAsync({entityName}UpdateDto {entityName.ToLower()}UpdateDto, CancellationToken cancellationToken);
+            Task<IList<{entityName}Dto>> GetAllAsync(Expression<Func<{entityName}, bool>> predicate, Func<IQueryable<{entityName}>, IIncludableQueryable<{entityName}, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default);            
+            Task<IDataResult<{entityName}Dto>> AddAsync({entityName}AddDto {camelCaseEntityName}AddDto, CancellationToken cancellationToken);
+            Task<IDataResult<{entityName}Dto>> UpdateAsync({entityName}UpdateDto {camelCaseEntityName}UpdateDto, CancellationToken cancellationToken);
             Task<IResult> DeleteAsync(Guid id, CancellationToken cancellationToken);
-        }}";
+        }}
+    }}";
     }
 
     static string GenerateClass(string entityName, string moduleName)
     {
+        var camelCaseEntityName = ToCamelCase(entityName);
+
         return $@"
-        using AutoMapper;
-        using Microsoft.EntityFrameworkCore.Query;   
-        using Microsoft.Extensions.Logging;
-        using MODISO.CORE.Utilities.Results.Abstract;
-        using MODISO.BLL.Abstract.{moduleName};
-        using MODISO.BLL.Repositories.{moduleName};
-        using MODISO.CORE.Utilities.Results.ComplexTypes;
-        using MODISO.CORE.Utilities.Results.Concrete;
-        using NArchitecture.Core.Persistence.Paging;
-        using MODISO.DOMAIN.DTOs.{moduleName}.{entityName}Dtos;
-        using MODISO.DOMAIN.Entities.{moduleName};
-        using System.Linq.Expressions;
-        using FluentValidation;
+    using AutoMapper;
+    using Microsoft.EntityFrameworkCore.Query;   
+    using Microsoft.Extensions.Logging;
+    using MODISO.CORE.Utilities.Results.Abstract;
+    using MODISO.BLL.Abstract.{moduleName};
+    using MODISO.BLL.Repositories.{moduleName};
+    using MODISO.CORE.Utilities.Results.ComplexTypes;
+    using MODISO.CORE.Utilities.Results.Concrete;
+    using NArchitecture.Core.Persistence.Paging;
+    using MODISO.DOMAIN.DTOs.{moduleName}.{entityName}Dtos;
+    using MODISO.DOMAIN.Entities.{moduleName};
+    using System.Linq.Expressions;
+    using FluentValidation;
 
-
-        namespace MODISO.BLL.Concrete.{moduleName};
-
+    namespace MODISO.BLL.Concrete.{moduleName}
+    {{
         public class {entityName}Service : I{entityName}Service
         {{
-            private readonly I{entityName}Repository _{entityName.ToLower()}Repository;
+            private readonly I{entityName}Repository _{camelCaseEntityName}Repository;
             private readonly IMapper _mapper;
             //private readonly {entityName}AddDtoValidator _validator;
             private readonly ILogger<{entityName}Service> _logger;
 
-            public {entityName}Service(I{entityName}Repository {entityName.ToLower()}Repository, IMapper mapper, /*{entityName}AddDtoValidator validator,*/ ILogger<{entityName}Service> logger)
+            public {entityName}Service(I{entityName}Repository {camelCaseEntityName}Repository, IMapper mapper, /*{entityName}AddDtoValidator validator,*/ ILogger<{entityName}Service> logger)
             {{
-                _{entityName.ToLower()}Repository = {entityName.ToLower()}Repository;
+                _{camelCaseEntityName}Repository = {camelCaseEntityName}Repository;
                 _mapper = mapper;
-              //_validator = validator;
+                //_validator = validator;
                 _logger = logger;
             }}
 
@@ -111,9 +115,9 @@
             {{
                 try
                 {{
-                    var {entityName.ToLower()} = await _{entityName.ToLower()}Repository.GetAsync(x => x.Id == id, cancellationToken: cancellationToken);
-                    var {entityName.ToLower()}Dto = _mapper.Map<{entityName}Dto>({entityName.ToLower()});
-                    return {entityName.ToLower()}Dto;
+                    var {camelCaseEntityName} = await _{camelCaseEntityName}Repository.GetAsync(x => x.Id == id, cancellationToken: cancellationToken);
+                    var {camelCaseEntityName}Dto = _mapper.Map<{entityName}Dto>({camelCaseEntityName});
+                    return {camelCaseEntityName}Dto;
                 }}
                 catch (Exception ex)
                 {{
@@ -127,9 +131,9 @@
             {{
                 try
                 {{
-                    var {entityName.ToLower()}s = await _{entityName.ToLower()}Repository.GetListAsync(index: index, size: size, cancellationToken: cancellationToken);
-                    var {entityName.ToLower()}Dtos = _mapper.Map<List<{entityName}Dto>>({entityName.ToLower()}s.Items);
-                    return {entityName.ToLower()}Dtos;
+                    var {camelCaseEntityName}s = await _{camelCaseEntityName}Repository.GetListAsync(index: index, size: size, cancellationToken: cancellationToken);
+                    var {camelCaseEntityName}Dtos = _mapper.Map<List<{entityName}Dto>>({camelCaseEntityName}s.Items);
+                    return {camelCaseEntityName}Dtos;
                 }}
                 catch (Exception ex)
                 {{
@@ -139,12 +143,28 @@
                 }}
             }}
 
+            public async Task<IList<{entityName}Dto>> GetAllAsync(Expression<Func<{entityName}, bool>> predicate, Func<IQueryable<{entityName}>, IIncludableQueryable<{entityName}, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+            {{
+                try
+                {{
+                    var {camelCaseEntityName}s = await _{camelCaseEntityName}Repository.GetListAsync(predicate: predicate, include: include, withDeleted: withDeleted, enableTracking: enableTracking, cancellationToken: cancellationToken);
+                    var {camelCaseEntityName}Dtos = _mapper.Map<List<{entityName}Dto>>({camelCaseEntityName}s.Items);
+                    return {camelCaseEntityName}Dtos;
+                }}
+                catch (Exception ex)
+                {{
+                    _logger.LogError(ex, ""Error in {{MethodName}}. Failed to get all {entityName}s. Predicate: {{Predicate}}, Include: {{Include}}, withDeleted: {{withDeleted}}, enableTracking: {{enableTracking. Details: {{ExceptionMessage}}"",
+                     nameof(GetAllAsync), predicate, include, withDeleted, enableTracking, ex.Message);
+                    return null;
+                }}
+            }}
+
             public async Task<{entityName}Dto?> GetAsync(Expression<Func<{entityName}, bool>> predicate, Func<IQueryable<{entityName}>, IIncludableQueryable<{entityName}, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
             {{
                 try
                 {{
-                    {entityName}? {entityName.ToLower()} = await _{entityName.ToLower()}Repository.GetAsync(predicate, include, withDeleted, enableTracking, cancellationToken);
-                    return _mapper.Map<{entityName}Dto>({entityName.ToLower()});
+                    {entityName}? {camelCaseEntityName} = await _{camelCaseEntityName}Repository.GetAsync(predicate, include, withDeleted, enableTracking, cancellationToken);
+                    return _mapper.Map<{entityName}Dto>({camelCaseEntityName});
                 }}
                 catch (Exception ex)
                 {{
@@ -158,8 +178,8 @@
             {{
                 try
                 {{
-                    IPaginate<{entityName}> {entityName.ToLower()}List = await _{entityName.ToLower()}Repository.GetListAsync(predicate, orderBy, include, index, size, withDeleted, enableTracking, cancellationToken);
-                    return _mapper.Map<Paginate<{entityName}Dto>>({entityName.ToLower()}List);
+                    IPaginate<{entityName}> {camelCaseEntityName}List = await _{camelCaseEntityName}Repository.GetListAsync(predicate, orderBy, include, index, size, withDeleted, enableTracking, cancellationToken);
+                    return _mapper.Map<Paginate<{entityName}Dto>>({camelCaseEntityName}List);
                 }}
                 catch (Exception ex)
                 {{
@@ -169,20 +189,20 @@
                 }}
             }}
 
-            public async Task<IDataResult<{entityName}Dto>> AddAsync({entityName}AddDto {entityName.ToLower()}AddDto, CancellationToken cancellationToken)
+            public async Task<IDataResult<{entityName}Dto>> AddAsync({entityName}AddDto {camelCaseEntityName}AddDto, CancellationToken cancellationToken)
             {{
                 try
                 {{
-                    if ({entityName.ToLower()}AddDto == null)
+                    if ({camelCaseEntityName}AddDto == null)
                         return new DataResult<{entityName}Dto>(ResultStatus.Error, ""{entityName} data cannot be null."", null);
 
-                   // var validationResult = _validator.Validate({entityName.ToLower()}AddDto);
-                   // if (!validationResult.IsValid)
-                   //    return new DataResult<{entityName}Dto>(ResultStatus.Error, ""Validation failed. Please check the provided data."", null);
+                    // var validationResult = _validator.Validate({camelCaseEntityName}AddDto);
+                    // if (!validationResult.IsValid)
+                    //    return new DataResult<{entityName}Dto>(ResultStatus.Error, ""Validation failed. Please check the provided data."", null);
 
-                    var {entityName.ToLower()} = _mapper.Map<{entityName}>({entityName.ToLower()}AddDto);
-                    await _{entityName.ToLower()}Repository.AddAsync({entityName.ToLower()});
-                    var resultData = _mapper.Map<{entityName}Dto>({entityName.ToLower()});
+                    var {camelCaseEntityName} = _mapper.Map<{entityName}>({camelCaseEntityName}AddDto);
+                    await _{camelCaseEntityName}Repository.AddAsync({camelCaseEntityName});
+                    var resultData = _mapper.Map<{entityName}Dto>({camelCaseEntityName});
                     return new DataResult<{entityName}Dto>(ResultStatus.Success, ""The {entityName} has been added successfully."", resultData);
                 }}
                 catch (ValidationException ex)
@@ -199,26 +219,26 @@
                 }}
             }}
 
-            public async Task<IDataResult<{entityName}Dto>> UpdateAsync({entityName}UpdateDto {entityName.ToLower()}UpdateDto, CancellationToken cancellationToken)
+            public async Task<IDataResult<{entityName}Dto>> UpdateAsync({entityName}UpdateDto {camelCaseEntityName}UpdateDto, CancellationToken cancellationToken)
             {{
                 try
                 {{
-                    if ({entityName.ToLower()}UpdateDto == null)
+                    if ({camelCaseEntityName}UpdateDto == null)
                         return new DataResult<{entityName}Dto>(ResultStatus.Error, ""{entityName} data cannot be null."", null);
 
-                    {entityName}? {entityName.ToLower()} = await _{entityName.ToLower()}Repository.GetAsync(x => x.Id == {entityName.ToLower()}UpdateDto.Id, cancellationToken: cancellationToken);
-                    if ({entityName.ToLower()} is null)
-                        return new DataResult<{entityName}Dto>(ResultStatus.Error, $""{entityName} with ID {{{{{entityName.ToLower()}UpdateDto.Id}}}} not found."", null);
+                    {entityName}? {camelCaseEntityName} = await _{camelCaseEntityName}Repository.GetAsync(x => x.Id == {camelCaseEntityName}UpdateDto.Id, cancellationToken: cancellationToken);
+                    if ({camelCaseEntityName} is null)
+                        return new DataResult<{entityName}Dto>(ResultStatus.Error, $""{entityName} with ID {{{{{camelCaseEntityName}UpdateDto.Id}}}} not found."", null);
 
-                    {entityName.ToLower()} = _mapper.Map({entityName.ToLower()}UpdateDto, {entityName.ToLower()});
-                    await _{entityName.ToLower()}Repository.UpdateAsync({entityName.ToLower()});
-                    var resultData = _mapper.Map<{entityName}Dto>({entityName.ToLower()});
+                    {camelCaseEntityName} = _mapper.Map({camelCaseEntityName}UpdateDto, {camelCaseEntityName});
+                    await _{camelCaseEntityName}Repository.UpdateAsync({camelCaseEntityName});
+                    var resultData = _mapper.Map<{entityName}Dto>({camelCaseEntityName});
                     return new DataResult<{entityName}Dto>(ResultStatus.Success, ""The {entityName} has been updated successfully."", resultData);
                 }}
                 catch (Exception ex)
                 {{
                     _logger.LogError(ex, ""Error in {{MethodName}}. An unexpected error occurred while updating the {entityName} with ID {{Id}}. Details: {{ExceptionMessage}}"",
-                        nameof(UpdateAsync), {entityName.ToLower()}UpdateDto?.Id, ex.Message);
+                        nameof(UpdateAsync), {camelCaseEntityName}UpdateDto?.Id, ex.Message);
                     return new DataResult<{entityName}Dto>(ResultStatus.Error, ""An unexpected error occurred. Please try again."", null);
                 }}
             }}
@@ -227,11 +247,11 @@
             {{
                 try
                 {{
-                    var {entityName.ToLower()} = await _{entityName.ToLower()}Repository.GetAsync(x => x.Id == id, cancellationToken: cancellationToken);
-                    if ({entityName.ToLower()} == null)
+                    var {camelCaseEntityName} = await _{camelCaseEntityName}Repository.GetAsync(x => x.Id == id, cancellationToken: cancellationToken);
+                    if ({camelCaseEntityName} == null)
                         return new Result(ResultStatus.Error, $""{entityName} with ID {{{{id}}}} not found."");
 
-                    await _{entityName.ToLower()}Repository.DeleteAsync({entityName.ToLower()}, cancellationToken: cancellationToken);
+                    await _{camelCaseEntityName}Repository.DeleteAsync({camelCaseEntityName}, cancellationToken: cancellationToken);
                     return new Result(ResultStatus.Success, ""The {entityName} has been deleted successfully."");
                 }}
                 catch (Exception ex)
@@ -241,7 +261,8 @@
                     return new Result(ResultStatus.Error, ""An unexpected error occurred. Please try again."");
                 }}
             }}
-        }}";
+        }}
+    }}";
     }
 
     static string GenerateMapper(string entityName, string moduleName)
@@ -266,4 +287,13 @@
             }}
         }}";
     }
+
+    public static string ToCamelCase(string str)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length < 2)
+            return str.ToLower();
+
+        return char.ToLower(str[0]) + str.Substring(1);
+    }
+
 }
